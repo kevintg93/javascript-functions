54009e266c1b2d09ed34601e73111882fbc68903
kevintg93 <52497336+kevintg93@users.noreply.github.com>
2021-11-20 09:42:52 +0200

    Add files via upload

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,27 +1,150 @@
-function seed() {}
+function seed(...arg) {return arg;}
 
-function same([x, y], [j, k]) {}
+function same([x, y], [j, k]) {
+  cell1= [x,y];
+  cell2=[j,k];
+  let result = cell1.every(element=> cell2.includes(element));
+  return result;
+}
 
 // The game state to search for `cell` is passed as the `this` value of the function.
-function contains(cell) {}
+function contains(cell) {
+  let y=[];
+      let z=[];
+      g=this;
+      g.forEach(element => { 
+        let temp = JSON.stringify(element)===JSON.stringify(cell);
+        if(temp===true){
+            y.push(temp);
+            z=element
+        }else{
+          y.push(temp);
+        }
+      })
+     let val= y.filter(x=>x===true);
+      if(val[0]===true){
+          return z;
+      }else{return undefined}
+}
 
-const printCell = (cell, state) => {};
 
-const corners = (state = []) => {};
+const printCell = (cell, state) => {
+  let value = contains.call(state,cell)
+    if (value !== undefined){
+      return '\u25A3';
+    }
+    else{
+      return '\u25A2';
+    }
+};
 
-const printCells = (state) => {};
+const corners = (state = []) => {
+  let xAxis=[];
+  let yAxis=[];
+  state.forEach(element=>{
+    xAxis.push(element[0])
+  });
+  state.forEach(element=>{
+    yAxis.push(element[1])
+  });
+  let topRight =[Math.max(...xAxis), Math.max(...yAxis)];
+  let bottomLeft =[Math.min(...xAxis), Math.min(...yAxis)];
+  if(state[0]!== undefined){
+    return {'topRight':topRight,'bottomLeft':bottomLeft}
+  }else{
+    return {'topRight':[0,0],'bottomLeft':[0,0]}
+  }
+};
 
-const getNeighborsOf = ([x, y]) => {};
+const printCells = (state) => {
+  let cornersValue= corners(state);
+  let bottomLeft =cornersValue.bottomLeft;
+  let topRight=cornersValue.topRight; 
+  let grid=[]
+ 
+ for(y=bottomLeft[1];y<=topRight[1];y++){
+     for(x=topRight[0];x>=bottomLeft[0];x--){
+             grid.push([x,y]);
+         }
+     }
+     reverseGrid=grid.reverse()
+     let cellGrid=[];
+     reverseGrid.forEach(element=>{
+       cellGrid.push(printCell(element,state));
+       })
+       for (i=0;i<cellGrid.length;i++){
+         if(i<cellGrid.length){
+           i=i+3
+           cellGrid.splice(i,0,'\n')
+         }
+       }
+       return cellGrid.join(' ')
+};
 
-const getLivingNeighbors = (cell, state) => {};
+const getNeighborsOf = ([x, y]) => {
+  var list = []
+  for (var i = (x-1); i <= (x+1); i++) {
+    for (var j = (y-1); j <= (y+1); j++) {
+    list.push([i,j]);
+    }
+}
+list = list.filter(element=> JSON.stringify(element) !== JSON.stringify([x,y]) )
+return list
+};
 
-const willBeAlive = (cell, state) => {};
+const getLivingNeighbors = (cell, state) => {
+  let neighbors = getNeighborsOf(cell);
+  let livingCells=[];
+  neighbors.forEach(element=>{
+    let aliveCells = contains.bind(state);
+    livingCells.push(aliveCells(element));
+  })
+  return livingCells.filter(x=>x!==undefined);
+};
 
-const calculateNext = (state) => {};
+const willBeAlive = (cell, state) => {
+  let livingNeighbors = getLivingNeighbors(cell,state);
+  let aliveCell = contains.bind(state)
+  if(livingNeighbors.length===3 || aliveCell!== undefined && livingNeighbors.length===2){
+    return cell
+  }
+};
 
-const iterate = (state, iterations) => {};
+const calculateNext = (state) => {
+  let grid= corners(state)
+  
+   grid.topRight.forEach((t,index)=> grid.topRight[index] = t+1);
+   grid.bottomLeft.forEach((s,index)=> grid.bottomLeft[index] = s-1);
+   let newGameState=[]
+   for(y=grid.bottomLeft[1];y<=grid.topRight[1];y++){
+    for(x=grid.topRight[0];x>=grid.bottomLeft[0];x--){
+            newGameState.push([x,y]);
+        }
+    }
+    let newLiveCell= newGameState.filter(element=>{ 
+      return (willBeAlive(element,state) !== undefined);
+    })
+    return newLiveCell;
+};
 
-const main = (pattern, iterations) => {};
+const iterate = (state, iterations) => {
+  let gameStates=[];
+  gameStates.push(state)
+  for(i=0;i<iterations;i++){
+    let temp = calculateNext(gameStates[i]);
+    gameStates.push(temp)
+  }
+  return gameStates;
+};
+
+const main = (pattern, iterations) => {
+  let gameStates=iterate(pattern,iterations);
+  let design =''
+  gameStates.forEach(element=>{ 
+    design += '\n' + printCells(element);
+  })
+  return design;
+};
 
 const startPatterns = {
     rpentomino: [
